0d9bb11c93fe5ce17646ff9a024e5c50c872f79b
dannymichel <dan@varmag.com>
2024-06-28 17:02:42 -0400

    database in home directory

--- scripts/db.js
+++ scripts/db.js
@@ -1,5 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./rbaker.db');
+const path = require('path');
+const os = require('os');
+
+const configDir = path.join(os.homedir(), '.config', 'rbaker');
+const dbPath = path.join(configDir, 'rbaker.db');
+
+const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS config (
